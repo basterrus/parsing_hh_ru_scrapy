68488e51a3649ecde1d3aa57c3451d80ebcc73ca
PavelNedoshivin <pnedoshivin@gmail.com>
2021-08-29 22:25:22 +0300

    Практическое задание к уроку 3

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import json
-
+from pymongo import MongoClient
 import requests
 from bs4 import BeautifulSoup
 import lxml
@@ -19,6 +19,11 @@ area_dict = {
 
 
 def main():
+    client = MongoClient('127.0.0.1', 27017)
+    db = client['hh']
+    vacancy = db.vacancy
+
+    global vacancy_dict
     vacancy_name = input('Введите название вакансии: ').lower()
     vacancy_area = input('Введите регион поиска: ').lower()
     if vacancy_area in area_dict:
@@ -39,35 +44,32 @@ def main():
         all_vacancy_info = soup.find_all('div', {'class': 'vacancy-serp-item'})
 
         try:
-            vacancy_dict = []
+            vacancy_dict = {}
             for vacancy in all_vacancy_info:
                 vacancy_card = vacancy.find('a')
                 vacancy_name = vacancy_card.text
-                vacancy_href = vacancy_card.get('href')
+                vacancy_href = str(vacancy_card.get('href'))
                 sal_info = vacancy.find('span', {'data-qa': 'vacancy-serp__vacancy-compensation'}).text.split(' ')
                 sal_cur = sal_info[-1]
                 if sal_info[0] == 'от':
-                    sal_min = sal_info[1].replace(u'\u202f', '')
+                    sal_min = int(sal_info[1].replace(u'\u202f', ''))
                     sal_max = None
                 elif sal_info[0] == 'до':
                     sal_min = None
-                    sal_max = int(sal_info[1]).replace(u'\u202f', '')
+                    sal_max = int(sal_info[1].replace(u'\u202f', ''))
                 else:
                     sal_min = sal_info[0].replace(u'\u202f', '')
                     sal_max = sal_info[2].replace(u'\u202f', '')
 
-                vacancy_dict.append({
-                    'Название вакансии': vacancy_name,
-                    'Ссылка на вакансию': vacancy_href,
-                    'Минимальная зарплата': sal_min,
-                    'Максимальная зарплата': sal_max,
-                    'Валюта': sal_cur,
-                })
-
                 print(f'Сохраняем данные страницы {page} в JSON файл')
-                with open('vacancy_data.json', "a", encoding="utf-8") as file:
-                    json.dump(vacancy_dict, file, indent=4, ensure_ascii=False)
+
                 page += 1
+
+                vacancy_dict['vacancy_name'] = vacancy_name
+                vacancy_dict['vacancy_href'] = vacancy_href
+                vacancy_dict['sal_min'] = sal_min
+                vacancy_dict['sal_max'] = sal_max
+                vacancy_dict['currency'] = sal_cur
 
         except:
             sal_min = None
@@ -75,7 +77,10 @@ def main():
             sal_cur = None
 
         if not soup.find(text='дальше'):
+            print('Cохранение завершено!')
             break
+
+    print(vacancy_dict)
 
 
 if __name__ == '__main__':
